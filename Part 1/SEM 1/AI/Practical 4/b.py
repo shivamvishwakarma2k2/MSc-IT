70b18ae3270943572c8a2c99147165e37ef4c9a4
shivamvishwakarma2k2 <138270712+shivamvishwakarma2k2@users.noreply.github.com>
2024-09-17 22:00:09 +0530

    updated on 17-09-2024

--- Part 1/SEM 1/AI/Practical 4/b.py
+++ Part 1/SEM 1/AI/Practical 4/b.py
@@ -1,7 +1,7 @@
 from pyamaze import maze,agent,COLOR
 
-m=maze(7,10)
-m.CreateMaze( pattern='v', loopPercent=40, theme=COLOR.dark)
-a=agent(m,filled=True,shape = 'arrow', footprints=True, color='red')
+m = maze(10, 15)
+m.CreateMaze(pattern='v', loopPercent=50, theme=COLOR.light)
+a = agent(m, filled=True,shape = 'arrow', footprints=True, color='red')
 m.tracePath({a:m.path})
 m.run()
